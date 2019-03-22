fa802587813a8796fee1a8f1d0405277493084e1
Lucas Xavier Leite <lucasxavierleite@pm.me>
2019-03-22 18:28:08 -0300

    Print message and print intro subroutine. Menu nows uses printMsg (1200)

--- orgame.asm
+++ orgame.asm
@@ -29,19 +29,116 @@
 ;
 
 ;sample : string "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-menu1   : string " @@@@@                                  "
-menu2   : string " @   @  @@@@   @@@    @@@   @    @ @@@@ "
-menu3   : string " @   @  @   @ @   @  @   @  @@  @@ @    "
-menu4   : string " @   @  @   @ @      @   @  @ @@ @ @@@  "
-menu5   : string " @   @  @@@@  @  @@  @@@@@  @    @ @    "
-menu6   : string " @   @  @  @  @   @  @   @  @    @ @    "
-menu7   : string " @@@@@  @   @  @@@   @   @  @    @ @@@@ "
-menu8   : string "                                        "
-menu9   : string "                                        "
-menu10  : string "                                        "
-menu11  : string "                                        "
-menu12  : string "                                        "
-menu13  : string "             <PRESS ENTER>              "
+menu1   : string "                                        "
+menu2   : string "                                        "
+menu3   : string "                                        "
+menu4   : string "                                        "
+menu5   : string "                                        "
+menu6   : string "                                        "
+menu7   : string "                                        "
+menu8   : string " @@@@@                                  "
+menu9   : string " @   @  @@@@   @@@    @@@   @    @ @@@@ "
+menu10  : string " @   @  @   @ @   @  @   @  @@  @@ @    "
+menu11  : string " @   @  @   @ @      @   @  @ @@ @ @@@  "
+menu12  : string " @   @  @@@@  @  @@  @@@@@  @    @ @    "
+menu13  : string " @   @  @  @  @   @  @   @  @    @ @    "
+menu14  : string " @@@@@  @   @  @@@   @   @  @    @ @@@@ "
+menu15  : string "                                        "
+menu16  : string "                                        "
+menu17  : string "                                        "
+menu18  : string "                                        "
+menu19  : string "                                        "
+menu20  : string "             <PRESS ENTER>              "
+menu21  : string "                                        "
+menu22  : string "                                        "
+menu23  : string "                                        "
+menu24  : string "                                        "
+menu25  : string "                                        "
+menu26  : string "                                        "
+menu27  : string "                                        "
+menu28  : string "                                        "
+menu29  : string "                                        "
+menu30  : string "                                        "
+
+;*******************************************************************************
+
+;******** messages *************************************************************
+;
+;  Messages. Game story etc.
+;
+
+
+;*** intro ****************************************************
+;
+;  Story intro. Text not defined yet
+;  This text is just a placeholder
+;
+
+;sample    : string "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+introLine1  : string "                                        "
+introLine2  : string "                                        "
+introLine3  : string "                                        "
+introLine4  : string "                                        "
+introLine5  : string "                                        "
+introLine6  : string "                                        "
+introLine7  : string " A long time ago, in a fantasy land...  "
+introLine8  : string "                                        "
+introLine9  : string "                                        "
+introLine10 : string "                                        "
+introLine11 : string "                                        "
+introLine12 : string "                                        "
+introLine13 : string "                                        "
+introLine14 : string "                                        "
+introLine15 : string "                                        "
+introLine16 : string "                                        "
+introLine17 : string "                                        "
+introLine18 : string "                                        "
+introLine19 : string "                                        "
+introLine20 : string "                                        "
+introLine21 : string "                                        "
+introLine22 : string "                                        "
+introLine23 : string "                                        "
+introLine24 : string "                                        "
+introLine25 : string "                                        "
+introLine26 : string "                                        "
+introLine27 : string "                                        "
+introLine28 : string "                                        "
+introLine29 : string "                                        "
+introLine30 : string "                                        "
+
+intro2Line1  : string "                                        "
+intro2Line2  : string "                                        "
+intro2Line3  : string "                                        "
+intro2Line4  : string "                                        "
+intro2Line5  : string "                                        "
+intro2Line6  : string "                                        "
+intro2Line7  : string "  There was a magic mushroom living in  "
+intro2Line8  : string "               a tree.                  "
+intro2Line9  : string "                                        "
+intro2Line10 : string "                                        "
+intro2Line11 : string "                                        "
+intro2Line12 : string " No, just kidding. That's not the story "
+intro2Line13 : string "                here.                   " 
+intro2Line14 : string "                                        "
+intro2Line15 : string "                                        "
+intro2Line16 : string "                                        "
+intro2Line17 : string "                                        "
+intro2Line18 : string "                                        "
+intro2Line19 : string "                                        "
+intro2Line20 : string "                                        "
+intro2Line21 : string "                                        "
+intro2Line22 : string "                                        "
+intro2Line23 : string "                                        "
+intro2Line24 : string "                                        "
+intro2Line25 : string "                                        "
+intro2Line26 : string "                                        "
+intro2Line27 : string "                                        "
+intro2Line28 : string "                                        "
+intro2Line29 : string "                                        "
+intro2Line30 : string "                                        "
+
+;***************************************************************
+
 
 ;*******************************************************************************
 
@@ -89,10 +186,13 @@ mapLine29 : string "@     @@@@@@         @@@@@@@@@@@@ @@@@@@"
 mapLine30 : string "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 
 
+;*******************************************************************************
+
+
 ;******** main *****************************************************************
 ;
 ;  This is the main section. The subroutines are called from here.
-;  Ex: splashs screen, main menu, print map, loop, game over, roll credits etc.
+;  Ex: splash screen, main menu, print map, loop, game over, roll credits etc.
 ;
 
 main:
@@ -104,6 +204,7 @@ main:
 	add r1, r1, r2 ; add color
 
 	call menu
+	call printIntro
 	call printMap
 	call loop
 
@@ -111,10 +212,58 @@ main:
 
 ;******** menu *****************************************************************
 ;
-;  Prints the initial menu. Press start to continue
+;  Prints the initial menu. Press enter to continue
 ;
 
 menu:
+	push fr
+	push r1
+
+	loadn r1, #menu1
+	call printMsg
+
+	pop r1
+	pop fr
+	rts
+
+;*******************************************************************************
+
+
+;******** printIntro ***********************************************************
+;
+;  Prints the introduciton to the game story, using the 'printMsg' subroutine.
+;
+
+printIntro:
+	push fr
+	push r1
+
+	loadn r1, #introLine1
+	call printMsg
+	loadn r1, #intro2Line1
+	call printMsg
+
+	pop r1
+	pop fr
+	rts
+
+
+;*******************************************************************************
+
+
+;******** printMsg ***********************************************************
+;
+;  Prints the message, using the 'printBlock' subroutine. Press start to
+;  continue
+;
+;  Parameters:
+;    r1 : message to be printed
+;
+;  Registers:
+;    r0, r2 .. r5 : 'printBlock' parameters
+;
+
+printMsg:
 	push fr
 	push r0
 	push r1
@@ -123,18 +272,19 @@ menu:
 	push r4
 	push r5
 
-	loadn r0, #240   ; menu initial position
-	loadn r1, #menu1 ; menu first line
-	loadn r2, #0     ; menu default color
-	loadn r3, #760   ; last character position 13 * 40 + 240 = 760
-	call printMapLine
+	loadn r0, #0
+	loadn r2, #0
+	loadn r3, #1200
+	call printBlock
 
 	loadn r4, #13 ; enter
 
-menuLoop:
+printMsgLoop:
 	inchar r5
 	cmp r5, r4
-	jne menuLoop
+	jne printMsgLoop
+
+	call clear
 
 	pop r5
 	pop r4
@@ -144,8 +294,6 @@ menuLoop:
 	pop r0
 	pop fr
 	rts
-
-;*******************************************************************************
 
 
 ;*******************************************************************************
@@ -177,9 +325,9 @@ printc:
 ;*******************************************************************************
 
 
-;******** printMapLine *********************************************************
+;******** printBlock ***********************************************************
 ;
-;  Prints the map lines
+;  Prints the memory block (specified size)
 ;
 ;  Registers:
 ;    r0: current position
@@ -190,7 +338,7 @@ printc:
 ;    r5: temporary
 ;
 
-printMapLine:
+printBlock:
 	push fr
 	push r0
 	push r1
@@ -199,9 +347,11 @@ printMapLine:
 	push r4
 	push r5
 
-printMapLineLoop:
+printBlockLoop:
 	loadi r4, r1
-	
+
+;**** map specifics ****
+
 	loadn r5, #'@'
 	cmp r4, r5
 	ceq printWall
@@ -214,6 +364,8 @@ printMapLineLoop:
 	cmp r4, r5
 	ceq printNext
 
+;***********************
+
 	loadn r5, #'\0'
 	cmp r4, r5
 	jeq breakln
@@ -221,7 +373,7 @@ printMapLineLoop:
 	call printc
 
 	cmp r0, r3
-	jne printMapLineLoop
+	jne printBlockLoop
 
 	pop r5
 	pop r4
@@ -235,14 +387,44 @@ printMapLineLoop:
 
 breakln:
 	inc r1
-	jmp printMapLineLoop
+	jmp printBlockLoop
+
+;*******************************************************************************
+
+
+;******** clear ****************************************************************
+;
+;  Clear the screen [0 .. 1200) by printing blank spaces.
+;
+
+clear:
+	push fr
+	push r0
+	push r1
+	push r2
+
+	loadn r0, #0
+	loadn r1, #1200
+	loadn r2, #' '
+
+clearLoop:
+	outchar r2, r0
+	inc r0
+	cmp r0, r1
+	jne clearLoop
+
+	pop r2
+	pop r1
+	pop r0
+	pop fr
+	rts
 
 ;*******************************************************************************
 
 
 ;******** printMap *************************************************************
 ;
-;  Prints the current map, using the "printMapLine" subroutine
+;  Prints the current map, using the "printBlock" subroutine
 ;
 ;  Registers:
 ;    r0: first character position
@@ -264,7 +446,7 @@ printMap:
 	loadn r1, #mapLine1
 	loadn r2, #2048
 	loadn r3, #1200
-	call printMapLine
+	call printBlock
 
 	pop r3
 	pop r2
@@ -279,7 +461,7 @@ printMap:
 ;******** objects colors *******************************************************
 ;
 ;  These subroutines loads the object respective color into the register r2,
-;    used by the "printMapLine" subroutine
+;  used by the "printBlock" subroutine
 ;
 ;  (see color table, at the top of this file)
 ;
@@ -333,12 +515,6 @@ loop:
 	loadn r6, #'d'
 	cmp r2, r6
 	ceq moveRight
-
-	;loadi r5, r0
-	;loadn r4, #mapLine1
-	;loadn r6, #0
-	;add r4, r4, r5
-	;outchar r4, r6
 
 	jmp loop
 	rts
